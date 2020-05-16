299f8c778c007135ce8a1adba922e0924fb5a2e6
Shan <shan.mi.fanfan@gmail.com>
2020-05-17 00:15:25 +0200

    some preparation

--- js/sortingHat.js
+++ js/sortingHat.js
@@ -3,13 +3,18 @@ const sortMeAreaWrapper = document.querySelector('.main-content__wrapper')
 const sortingHat = document.querySelector('.sorting-hat')
 const avatarMouth = document.querySelector('.avatar__mouth')
 const houses = ["hufflepuff", "gryffindor", "ravenclaw", "slytherin"]
+const leftEye = document.querySelector('.avatar__eyes.left')
+const rightEye = document.querySelector('.avatar__eyes.right')
+const mouth = document.querySelector('.avatar__mouth')
+
 let item = houses[Math.floor(Math.random() * houses.length)];
 let sortingHatAnswer = document.querySelector('.sorting-hat__answer')
-let genderSelector = document.querySelector('input[name=avatar-gender]')
+let genderSelectors = document.querySelectorAll('input[name=avatar-gender]')
 let hair = document.querySelector('.avatar__hair')
 
 sortMeBtn.addEventListener('click', () => {
-  sortMeAreaWrapper.setAttribute('class', '')
+  // sortMeAreaWrapper.setAttribute('class', '')
+  sortMeAreaWrapper.removeAttribute('class')
   sortMeAreaWrapper.classList.add('main-content__wrapper')
   sortingHat.classList.remove('animate')
   avatarMouth.classList.remove('animate')
@@ -30,12 +35,20 @@ sortMeBtn.addEventListener('click', () => {
   }, 4000);
 })
 
-genderSelector.addEventListener('change', () => {
-  let value = genderSelector.querySelector('checked').value
-  value == 1 ? hair.classList.remove('female') : hair.classList.add('female')
+genderSelectors.forEach(genderSelector => {
+  genderSelector.addEventListener('change', (e) => {
+    // console.log(e.currentTarget)
+    let value = e.currentTarget.value
+    // console.log(value)
+    value == 1 ? hair.classList.remove('female') : hair.classList.add('female')
+    value == 1 ? leftEye.classList.remove('female') : leftEye.classList.add('female')
+    value == 1 ? rightEye.classList.remove('female') : rightEye.classList.add('female')
+    value == 1 ? mouth.style.background = "#333" : mouth.style.background = "#e17055"
+
+  })
 })
 
 const sortingHatAnchor = document.querySelector('#sorting-hat-anchor')
 setTimeout(function () {
   sortingHatAnchor.scrollIntoView();
-}, 2000) //after 2 seconds, meet the sorting hat
+}, 4000) //after 4 seconds, meet the sorting hat
