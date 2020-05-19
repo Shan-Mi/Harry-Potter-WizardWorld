6434237fb772fa8bb370592c1db57f4b265c77dd
Shan <shan.mi.fanfan@gmail.com>
2020-05-19 21:32:56 +0200

    added more question to 15 question in total

--- js/quiz.js
+++ js/quiz.js
@@ -38,7 +38,59 @@ const questions = [{
     question: "How many times was Nearly Headless Nick hit in the neck with a blunt axe?",
     choices: ["25", "35", "45", "55"],
     correct: 2
-  }
+  },
+  /* 6 - 10 */
+  {
+    question: "Who is Harry Potter's godfather?",
+    choices: ["Sirius Black", "Albus Dumbledore", "Remus Lupin", "Arthur Weasley"],
+    correct: 0
+  },
+  {
+    question: "Which class has a different teacher every year? ",
+    choices: ["Charms", "Astronomy and Herbology", "Defence Against the Dark Arts", " History of Magic"],
+    correct: 2
+  },
+  {
+    question: "What animal represents Hufflepuff house?",
+    choices: ["Lion", "Badger", "Eagle", "Serpent"],
+    correct: 1
+  },
+  {
+    question: "What is the name of Hermione's pet cat? ",
+    choices: ["Fluffy", "Norbert", "Fang", "Crookshanks"],
+    correct: 3
+  },
+  {
+    question: "Who created the Sorting Hat?",
+    choices: ["The founders of Hogwarts", "Elf", "DeathEater", "Lord Voldemort"],
+    correct: 0
+  },
+  /* 11 - 15 */
+  {
+    question: "Where is Azkaban located?" ,
+    choices: ["The Burrow", "High Street", "The North Sea", "The West Sea"],
+    correct: 2
+  },
+  {
+    question: "And what platform do students need to catch it from at King's Cross?" ,
+    choices: ["Platform 9 1/4", "Platform 9 2/4", "Platform 9 3/4", "Platform 9 3/2"],
+    correct: 2
+  },
+  {
+    question: "What is the name of the device that Dumbledore uses when putting out street lights?" ,
+    choices: ["Deluminator", "Howler", "Revealer", "Remembrall"],
+    correct: 0
+  },
+  {
+    question: "What position does Harry play on the Quidditch team?" ,
+    choices: ["Chaser", "Beater", "Seeker", "Keeper"],
+    correct: 2
+  },
+  {
+    question: "Which type of insect is Ronald Weasley most afraid of?" ,
+    choices: ["Wasp", "Spider", "Centipede", "Croach"],
+    correct: 1
+  },
 ];
 
 window.onload = beginQuiz;
@@ -64,8 +116,8 @@ startBtn.addEventListener("click", function () {
 
 submitBtn.addEventListener("click", function () {
   allRadios = document.getElementsByName("option");
-  var isChecked = false;
-  for (var j = 0; j < allRadios.length; j++) {
+  let isChecked = false;
+  for (let j = 0; j < allRadios.length; j++) {
     if (allRadios[j].checked) {
       isChecked = true;
       checkedRadio = j;
@@ -85,7 +137,7 @@ submitBtn.addEventListener("click", function () {
 
 function deselectRadios() {
   allRadios = document.getElementsByName("option");
-  for (var p = 0; p < allRadios.length; p++) {
+  for (let p = 0; p < allRadios.length; p++) {
     allRadios[p].checked = false;
   }
 }
@@ -100,7 +152,7 @@ function getResults() {
 function getQAs() {
   if (i < 5) {
     askQuestion.innerHTML = questions[i].question;
-    for (var k = 0; k < 4; k++) {
+    for (let k = 0; k < 4; k++) {
       document.getElementById("answer" + k).innerHTML = questions[i].choices[k];
       document.getElementById("answer" + k).setAttribute("for", questions[i].choices[k]);
       document.getElementById("q" + k).querySelector(['input']).setAttribute("value", questions[i].choices[k]);
