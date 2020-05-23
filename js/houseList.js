da9ef8ab767ade9d8ac36c32e862b3a2cec42a5e
Shan <shan.mi.fanfan@gmail.com>
2020-05-23 14:31:45 +0200

    some responsive improvement

--- js/houseList.js
+++ js/houseList.js
@@ -158,7 +158,59 @@ listWrapperArea.addEventListener('click', (e) => {
   }
 })
 
+/* Go back to top btn */
 const goBackToFrontPageBtn = document.querySelector('.go-to-fp')
 goBackToFrontPageBtn.addEventListener('click', () => {
   window.location = "index.html"
 })
+
+/* Go back to top function for mobile*/
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 300) { // Show backToTopButton
+    if (!backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnExit");
+      backToTopButton.classList.add("btnEntrance");
+      backToTopButton.style.display = "block";
+    }
+  } else { // Hide backToTopButton
+    if (backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnEntrance");
+      backToTopButton.classList.add("btnExit");
+      setTimeout(function () {
+        backToTopButton.style.display = "none";
+      }, 250);
+    }
+  }
+}
+
+backToTopButton.addEventListener("click", smoothScrollBackToTop);
+
+function smoothScrollBackToTop() {
+  const targetPosition = 0;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 750;
+  let start = null;
+
+  window.requestAnimationFrame(step);
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+}
+
+function easeInOutCubic(t, b, c, d) {
+  t /= d / 2;
+  if (t < 1) return c / 2 * t * t * t + b;
+  t -= 2;
+  return c / 2 * (t * t * t + 2) + b;
+};
+
+document.querySelector('.date').innerHTML = new Date()
